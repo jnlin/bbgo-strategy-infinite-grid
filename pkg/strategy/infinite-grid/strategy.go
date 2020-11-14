3fc13c6855a92be588ea0874f5fe10cbf7982643
Jui-Nan Lin <jnlinn@gmail.com>
2020-11-14 11:52:42 +0800

    fix: remove tradeUpdateHandler

--- pkg/strategy/infinite-grid/strategy.go
+++ pkg/strategy/infinite-grid/strategy.go
@@ -126,12 +126,6 @@ func (s *Strategy) placeInfiniteGridOrders(orderExecutor bbgo.OrderExecutor, ses
 	s.activeOrders.Add(createdOrders...)
 }
 
-func (s *Strategy) tradeUpdateHandler(trade types.Trade) {
-	if trade.Symbol != s.Symbol {
-		return
-	}
-}
-
 func (s *Strategy) orderUpdateHandler(order types.Order) {
 	if order.Symbol != s.Symbol {
 		return
@@ -161,7 +155,6 @@ func (s *Strategy) Run(ctx context.Context, orderExecutor bbgo.OrderExecutor, se
 	})
 
 	session.Stream.OnOrderUpdate(s.orderUpdateHandler)
-	session.Stream.OnTradeUpdate(s.tradeUpdateHandler)
 	session.Stream.OnConnect(func() {
 		s.placeInfiniteGridOrders(orderExecutor, session)
 	})
